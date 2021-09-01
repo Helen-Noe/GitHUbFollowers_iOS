5a9c5008e44b31b1fa62c8193aa9bd31d5d5892e
Helen-Noe <helenmyatnoe@gmail.com>
2021-09-01 11:07:35 +1000

    completed search ui

---
+++ GitHubFollowers/FavouritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Thin Myat Noe on 29/8/21.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		view.backgroundColor = .systemBlue
+    }
+
+}
